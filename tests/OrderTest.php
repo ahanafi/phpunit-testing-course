d14d0197721b0fd3a7e5a6186ff41ba4f3804c67
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-01-29 07:59:21 +0700

    Add testOrderIsProcessedUsingMockery

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -4,7 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
 {
-    public function testOrderIsProcessed()
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testOrderIsProcessed(): void
     {
         $gateway = $this->getMockBuilder('PaymentGateway')
             ->setMethods(['charge'])
@@ -14,6 +19,20 @@ class OrderTest extends TestCase
             ->method('charge')
             ->with($this->equalTo(200))
             ->willReturn(true);
+
+        $order = new Order($gateway);
+        $order->amount = 200;
+        self::assertTrue($order->proccess());
+    }
+
+    public function testOrderIsProcessedUsingMockery(): void
+    {
+        $gateway = Mockery::mock('PaymentGateway');
+
+        $gateway->shouldReceive('charge')
+            ->once()
+            ->with(200)
+            ->andReturn(true);
 
         $order = new Order($gateway);
         $order->amount = 200;
